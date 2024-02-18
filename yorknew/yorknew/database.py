02ee75c6f50c5d7b74a2707bcf29b55cb003c15e
Brion Ye <brionqiye@gmail.com>
2024-02-17 19:07:03 -0800

    added more database ops

--- yorknew/yorknew/database.py
+++ yorknew/yorknew/database.py
@@ -2,6 +2,16 @@ from rxconfig import config
 import reflex as rx
 from typing import List
 
+entrycolumns = ["Rating", "User", "Caption"]
+
+# Database for the rankings of each caption
+class Entry(rx.Model, table=True):
+	subject: int
+    name: str
+    caption: str
+	rating: int
+
+
 # State for updating the current panel being observed
 class State(rx.State):
 	ratingspage: int
@@ -9,21 +19,25 @@ class State(rx.State):
 
 	# For general purpose
 	captions_set: list[Entry]
+	leaderboard_table: list[list]
 
 	# For rating systems
 	test_caption_1: Entry
 	test_caption_2: Entry
-	test_caption_custom: Entry
 
-	def get_captions_for_subject(self, subject):
+	def convert_entry_to_list(entry):
+		return [entry.rating, entry.user, entry.caption]
+
+	def get_all_captions_for_subject(self, subject):
         with rx.session() as session:
-            self.captions = session.exec(
+            self.captions_set = session.exec(
                 Entry.select.where(
                     Entry.subject == subject
                 ).all()
             )
 	
-	def get_two_captions_for_subject(self, subject):
+	
+	def load_two_captions_for_subject(self, subject):
         with rx.session() as session:
             self.users = session.exec(
                 Entry.select.where(
@@ -33,18 +47,23 @@ class State(rx.State):
 	
 	def update_captions_rating(self, caption_1_new_r, caption_2_new_r):
 		with rx.session() as session:
-            session.add(
-                User(
-                    username=self.username, email=self.email
-                )
-            )
+			test_caption_1.rating = caption_1_new_r
+            session.add(test_caption_1)
+			test_caption_2.rating = caption_2_new_r
+			session.add(test_caption_2)
 			session.commit()
-
-
-# Database for the rankings of each caption
-class Entry(rx.Model, table=True):
-	subject: int
-    name: str
-    caption: str
-	rating: int
+	
+	def add_new_caption(self, subject, name, caption):
+		with rx.session() as session:
+			session.add(
+				Entry(
+					subject=self.subject,
+					name=self.name,
+                    caption=self.caption, 
+					rating=self.rating,
+                )
+			)
+			session.commit()
+	
+	
 
